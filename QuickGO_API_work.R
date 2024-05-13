dd319038eee123a50bfadf41a9d7901f7e38fbdd
Iggi-29 <ignasijs29@gmail.com>
2024-05-13 09:56:15 +0200

    Script for the QuickGO API updated

--- QuickGO_API_work.R
+++ QuickGO_API_work.R
@@ -1,46 +1,23 @@
 #########################################
-### Reactome annotation and collapse ###
+### Get annotations from QuickGO API ###
 ########################################
 
 ### Load libraries and data
 ## Libraries
 library(rstudioapi)
 setwd(dirname(getActiveDocumentContext()$path))
-library(ggplot2)
-library(ggfortify)
-library(limma)
-library(gplots)
-library(pheatmap)
-library(RColorBrewer)
-library(stringr)
-library(janitor)
-library(dplyr)
-library(tidyr)
-library(mice)
-library(reshape2)
-library(openxlsx)
-library(NbClust)
-library(factoextra)
-library(readr)
-library("org.Hs.eg.db")
-library(clusterProfiler)
-library(scales)
-library(grid)
-library(gridExtra)
-library(cowplot)
 library(httr)
 library(jsonlite)
 library(xml2)
 
 ## Functions
-source("./functions/collapseGO.R")
-source("./functions/minestrone.R")
-source("./functions/treemaping.R")
-source("./functions/CollapseReactome.R")
 source("./functions/get_go_genes.R")
+source("./functions/get_child_terms.R")
 
 ## Data
+# Data could also be simply a string vector of GO terms to extract info from 
 GOs <- openxlsx::read.xlsx("./skeletal_muscle.xlsx")
+
 
 skeletal_proteins <- get_go_genes(go_id = GOs$Term, 
                                go_usage = "descendants", proteome = "gcrpCan", assigned_by = c("Uniprot","InterPro","GO_Central","BHF-UCL"), 
