ef9203a2551deb585ed24a0addc45beda39b26a4
Iggi-29 <ignasijs29@gmail.com>
2024-07-11 13:17:04 +0200

    removed a + symbol that caused the code to crash

--- GO_BP_and_collapse.R
+++ GO_BP_and_collapse.R
@@ -73,7 +73,7 @@ BP_go1 <- enrichGO(gene = c1.go$SYMBOL,
 
 ## Filter by count
 BP_go1 <- gsfilter(BP_go1, by = "Count", min = 5)
-+
+
 ## Write the data
 openxlsx::write.xlsx(x = BP_go1@result, file = "./results/go_bp/go_bp1.xlsx")
 
