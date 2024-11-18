b1932537e73295cf75cca5f5e6923922731487d2
Iggi-29 <ignasijs29@gmail.com>
2024-11-18 23:40:59 +0100

    Pfam petition function done

--- functions/get_interpro_pfam_data.R
+++ functions/get_interpro_pfam_data.R
@@ -3,8 +3,11 @@
 #######################
 
 library(httr)
+library(jsonlite)
+library(tidyr)
 
 ## protein_id = Uniprot protein ID
+## taxon = taxon id
 
 get_interpro_pfam_data <- function(protein_id,taxon){
   
@@ -13,6 +16,7 @@ get_interpro_pfam_data <- function(protein_id,taxon){
   result_list <- list()
   for (i in 1:length(protein_id)){
     ### Protein ID part
+    protein_now <- protein_id[i]
     unique_protein <- protein_id[i]
     unique_protein <- paste0(unique_protein,"/")
 
@@ -29,68 +33,104 @@ get_interpro_pfam_data <- function(protein_id,taxon){
     stop_for_status(response)
     content <- httr::content(response, as = "text", encoding = "UTF-8")
     
-    ## Deparse the content of the json
-    if (content != "") {"
-      "} else if (content == "") {
-        next }
-    
-    ## Put all the json data to a single dataframe
-
-    ## Append the dataframe into the list
+    ### Deparse the content of the json
+    if (content != "") {
+      ## Put all the json data to a single dataframe
+      parsed_data <- fromJSON(content)
+      results <- parsed_data$results
+      # metadata
+      metadata <- as.data.frame(results$metadata)
+      metadata$protein_accession <- protein_now 
+      metadata <- metadata %>% 
+        dplyr::rename(pfam_accession = accession)
+      # proteins
+      proteins <- as.data.frame(do.call(rbind, lapply(results$proteins, function(x) {
+        protein_locations <- do.call(rbind, lapply(x$entry_protein_locations, function(y) {
+          cbind(protein_accession = x$accession,
+                protein_length = x$protein_length,
+                start = y$fragments[[1]]$start,
+                end = y$fragments[[1]]$end,
+                score = y$score)}))
+        protein_locations})))
+      proteins <- proteins %>% 
+        dplyr::mutate(protein_accession = toupper(protein_accession))
+      # taxon
+      taxa <- do.call(rbind, lapply(results$taxa, function(x) {
+        cbind(accession = x$accession, lineage = paste(x$lineage, collapse = ";"))
+      }))
+      taxa <- as.data.frame(taxa)
+      taxa$protein_accession <- protein_now
+      # merges
+      pfam_annotation <- metadata %>% 
+        dplyr::left_join(y = proteins, by = "protein_accession") %>%
+        dplyr::left_join(y = taxa, by = "protein_accession")
+      
+      ## Append the dataframe into the list
+      result_list[[i]] <- pfam_annotation
+      
+      
+    } else if (content == "") {
+        next } 
   }
+  if (length(result_list) > 0){
+    final_list <- as.data.frame(do.call(rbind, result_list))
+  }
+  else if (length(result_list) == 0) {
+    final_list <- "Not match found !"
+  }
+  
   # final_data_frame <- as.data.frame(do.call(rbind, final_list))
-  # return(final_data_frame)
-  return(content)
+  # return(pfam_annotation)
+  return(final_list)
 }
 
 
 a <- get_interpro_pfam_data(protein_id = c("P68871"), taxon = "9606")
-
+b <- get_interpro_pfam_data(protein_id = c("kjgsgalglj"), taxon = "9606")
+c <- get_interpro_pfam_data(protein_id = c("P81177","Q7A656"), taxon = "1280")
 
 ################################## RESPONSE FOR 
 # Load required package
-library(jsonlite)
-library(tidyr)
 
 # Your API response as a JSON string
-api_response <- '{"count":1,"next":null,"previous":null,"results":[{"metadata":{"accession":"PF07968","name":"Leukocidin/Hemolysin toxin family","source_database":"pfam","type":"domain","integrated":"IPR016183","member_databases":null,"go_terms":null},"proteins":[{"accession":"p09616","protein_length":319,"source_database":"reviewed","organism":"1280","in_alphafold":true,"entry_protein_locations":[{"fragments":[{"start":63,"end":313,"dc-status":"CONTINUOUS"}],"representative":true,"model":"PF07968","score":7.3e-88}]}],"taxa":[{"accession":"1280","lineage":["1","131567","2","1783272","1239","91061","1385","90964","1279","1280"],"source_database":"uniprot"}],"extra_fields":{"entry_id":null}}]}'
-api_response <- "{\"count\":2,\"next\":null,\"previous\":null,\"results\":[{\"metadata\":{\"accession\":\"PF00091\",\"name\":\"Tubulin/FtsZ family, GTPase domain\",\"source_database\":\"pfam\",\"type\":\"domain\",\"integrated\":\"IPR003008\",\"member_databases\":null,\"go_terms\":null},\"proteins\":[{\"accession\":\"p0a031\",\"protein_length\":390,\"source_database\":\"reviewed\",\"organism\":\"1280\",\"in_alphafold\":true,\"entry_protein_locations\":[{\"fragments\":[{\"start\":14,\"end\":174,\"dc-status\":\"CONTINUOUS\"}],\"representative\":false,\"model\":\"PF00091\",\"score\":2.3e-50}]}],\"taxa\":[{\"accession\":\"1280\",\"lineage\":[\"1\",\"131567\",\"2\",\"1783272\",\"1239\",\"91061\",\"1385\",\"90964\",\"1279\",\"1280\"],\"source_database\":\"uniprot\"}],\"extra_fields\":{\"entry_id\":null}},{\"metadata\":{\"accession\":\"PF12327\",\"name\":\"FtsZ family, C-terminal domain\",\"source_database\":\"pfam\",\"type\":\"domain\",\"integrated\":\"IPR024757\",\"member_databases\":null,\"go_terms\":null},\"proteins\":[{\"accession\":\"p0a031\",\"protein_length\":390,\"source_database\":\"reviewed\",\"organism\":\"1280\",\"in_alphafold\":true,\"entry_protein_locations\":[{\"fragments\":[{\"start\":222,\"end\":316,\"dc-status\":\"CONTINUOUS\"}],\"representative\":false,\"model\":\"PF12327\",\"score\":7.3e-31}]}],\"taxa\":[{\"accession\":\"1280\",\"lineage\":[\"1\",\"131567\",\"2\",\"1783272\",\"1239\",\"91061\",\"1385\",\"90964\",\"1279\",\"1280\"],\"source_database\":\"uniprot\"}],\"extra_fields\":{\"entry_id\":null}}]}"
-api_response <- a
-
-# Parse the JSON
-parsed_data <- fromJSON(api_response)
-
-# Extract the "results" section
-results <- parsed_data$results
-
-# Flatten "metadata"
-metadata <- as.data.frame(results$metadata)
-
-# Flatten "proteins" (nested structure)
-proteins <- do.call(rbind, lapply(results$proteins, function(x) {
-  protein_locations <- do.call(rbind, lapply(x$entry_protein_locations, function(y) {
-    cbind(protein_accession = x$accession,
-          protein_length = x$protein_length,
-          start = y$fragments[[1]]$start,
-          end = y$fragments[[1]]$end,
-          score = y$score)
-  }))
-  protein_locations
-}))
-
-# Flatten "taxa"
-taxa <- do.call(rbind, lapply(results$taxa, function(x) {
-  cbind(accession = x$accession, lineage = paste(x$lineage, collapse = ";"))
-}))
-
-# View the extracted dataframes
-print("Metadata:")
-print(metadata)
-
-print("Proteins:")
-print(proteins)
-
-print("Taxa:")
-print(taxa)
-
-
+# api_response <- '{"count":1,"next":null,"previous":null,"results":[{"metadata":{"accession":"PF07968","name":"Leukocidin/Hemolysin toxin family","source_database":"pfam","type":"domain","integrated":"IPR016183","member_databases":null,"go_terms":null},"proteins":[{"accession":"p09616","protein_length":319,"source_database":"reviewed","organism":"1280","in_alphafold":true,"entry_protein_locations":[{"fragments":[{"start":63,"end":313,"dc-status":"CONTINUOUS"}],"representative":true,"model":"PF07968","score":7.3e-88}]}],"taxa":[{"accession":"1280","lineage":["1","131567","2","1783272","1239","91061","1385","90964","1279","1280"],"source_database":"uniprot"}],"extra_fields":{"entry_id":null}}]}'
+# api_response <- "{\"count\":2,\"next\":null,\"previous\":null,\"results\":[{\"metadata\":{\"accession\":\"PF00091\",\"name\":\"Tubulin/FtsZ family, GTPase domain\",\"source_database\":\"pfam\",\"type\":\"domain\",\"integrated\":\"IPR003008\",\"member_databases\":null,\"go_terms\":null},\"proteins\":[{\"accession\":\"p0a031\",\"protein_length\":390,\"source_database\":\"reviewed\",\"organism\":\"1280\",\"in_alphafold\":true,\"entry_protein_locations\":[{\"fragments\":[{\"start\":14,\"end\":174,\"dc-status\":\"CONTINUOUS\"}],\"representative\":false,\"model\":\"PF00091\",\"score\":2.3e-50}]}],\"taxa\":[{\"accession\":\"1280\",\"lineage\":[\"1\",\"131567\",\"2\",\"1783272\",\"1239\",\"91061\",\"1385\",\"90964\",\"1279\",\"1280\"],\"source_database\":\"uniprot\"}],\"extra_fields\":{\"entry_id\":null}},{\"metadata\":{\"accession\":\"PF12327\",\"name\":\"FtsZ family, C-terminal domain\",\"source_database\":\"pfam\",\"type\":\"domain\",\"integrated\":\"IPR024757\",\"member_databases\":null,\"go_terms\":null},\"proteins\":[{\"accession\":\"p0a031\",\"protein_length\":390,\"source_database\":\"reviewed\",\"organism\":\"1280\",\"in_alphafold\":true,\"entry_protein_locations\":[{\"fragments\":[{\"start\":222,\"end\":316,\"dc-status\":\"CONTINUOUS\"}],\"representative\":false,\"model\":\"PF12327\",\"score\":7.3e-31}]}],\"taxa\":[{\"accession\":\"1280\",\"lineage\":[\"1\",\"131567\",\"2\",\"1783272\",\"1239\",\"91061\",\"1385\",\"90964\",\"1279\",\"1280\"],\"source_database\":\"uniprot\"}],\"extra_fields\":{\"entry_id\":null}}]}"
+# 
+# # Parse the JSON
+# parsed_data <- fromJSON(api_response)
+# 
+# # Extract the "results" section
+# results <- parsed_data$results
+# 
+# # Flatten "metadata"
+# metadata <- as.data.frame(results$metadata)
+# 
+# # Flatten "proteins" (nested structure)
+# proteins <- as.data.frame(do.call(rbind, lapply(results$proteins, function(x) {
+#   protein_locations <- do.call(rbind, lapply(x$entry_protein_locations, function(y) {
+#     cbind(protein_accession = x$accession,
+#           protein_length = x$protein_length,
+#           start = y$fragments[[1]]$start,
+#           end = y$fragments[[1]]$end,
+#           score = y$score)
+#   }))
+#   protein_locations
+# })))
+# 
+# 
+# # Flatten "taxa"
+# taxa <- do.call(rbind, lapply(results$taxa, function(x) {
+#   cbind(accession = x$accession, lineage = paste(x$lineage, collapse = ";"))
+# }))
+# 
+# # View the extracted dataframes
+# print("Metadata:")
+# print(metadata)
+# 
+# print("Proteins:")
+# print(proteins)
+# 
+# print("Taxa:")
+# print(taxa)
+# 
+# 
